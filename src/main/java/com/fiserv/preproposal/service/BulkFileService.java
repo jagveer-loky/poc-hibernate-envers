d0559eba7444b049ea476b81b8bdd5b21cf82548
F1ME76O <victor.silva@firstdatacorp.com.br>
2021-05-03 15:11:49 -0300

    [03/05/21] First Commit

---
+++ src/main/java/com/fiserv/preproposal/service/BulkFileService.java
@@ -0,0 +1,19 @@
+package com.fiserv.preproposal.service;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BulkFileService {
+
+    private static final Logger LOG = LogManager.getLogger(BulkFileService.class);
+
+    public void readBulk(){
+        System.out.println("Hello World");
+    }
+
+
+}
---
+++ src/test/java/com/fiserv/preproposal/PreproposalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiserv.preproposal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreproposalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
