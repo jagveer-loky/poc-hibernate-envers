ea2caebfd3925d18f0d727239eeeef3c530c2c9d
F1ME76O <lucas.faria@fiserv.com>
2021-07-02 11:37:22 -0300

    [02/07/2021] Swagger Doc

--- src/main/java/com/fiserv/preproposal/api/application/resource/ReportResource.java
+++ src/main/java/com/fiserv/preproposal/api/application/resource/ReportResource.java
@@ -3,6 +3,9 @@ package com.fiserv.preproposal.api.application.resource;
 import com.fiserv.preproposal.api.domain.dtos.*;
 import com.fiserv.preproposal.api.domain.service.ReportService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -41,13 +44,52 @@ public class ReportResource {
     @Operation(
             summary = "Return the Report One Information",
             description = "Return the Report One Information",
-            tags = "REPORT"
+            tags = "REPORT",
+            parameters = {
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "institution",
+                            required = true,
+                            description = "Filter Institution",
+                            example = "00000007"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "serviceContract",
+                            required = true,
+                            description = "Filter Service Contract",
+                            example = "149"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "initialDate",
+                            required = true,
+                            description = "Filter Initial Date",
+                            example = "2020-10-10"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "finalDate",
+                            required = true,
+                            description = "Filter Final Date",
+                            allowEmptyValue = true,
+                            example = "2021-09-11"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "status",
+                            required = true,
+                            description = "Filter Status List",
+                            allowEmptyValue = true,
+                            example = "[PRE1,PRE2]"
+                    )
+            }
     )
     @ApiResponses(value = {
             @ApiResponse(
                     responseCode = "200",
                     description = "Sucessful Operation",
-                    content = @Content(schema = @Schema(implementation = DReport1[].class))
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = DReport1.class)))
             )
     })
     @GetMapping(DReport1.NAME)
@@ -60,7 +102,57 @@ public class ReportResource {
         return reportService.getReport1(institution, serviceContract, initialDate, finalDate, (Objects.isNull(status) || status.isEmpty()) ? null : status);
     }
 
-
+    @Operation(
+            summary = "Return the Report Two Information",
+            description = "Return the Report Two Information",
+            tags = "REPORT",
+            parameters = {
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "institution",
+                            required = true,
+                            description = "Filter Institution",
+                            example = "00000007"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "serviceContract",
+                            required = true,
+                            description = "Filter Service Contract",
+                            example = "149"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "initialDate",
+                            required = true,
+                            description = "Filter Initial Date",
+                            example = "2020-10-10"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "finalDate",
+                            required = true,
+                            description = "Filter Final Date",
+                            allowEmptyValue = true,
+                            example = "2021-09-11"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "status",
+                            required = true,
+                            description = "Filter Status List",
+                            allowEmptyValue = true,
+                            example = "[PRE1,PRE2]"
+                    )
+            }
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Sucessful Operation",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = DReport2.class)))
+            )
+    })
     @GetMapping(DReport2.NAME)
     public List<DReport2> getReport2(@RequestParam final String institution,
                                      @RequestParam final String serviceContract,
@@ -71,6 +163,57 @@ public class ReportResource {
         return reportService.getReport2(institution, serviceContract, initialDate, finalDate, (Objects.isNull(status) || status.isEmpty()) ? null : status);
     }
 
+    @Operation(
+            summary = "Return the Report Three Information",
+            description = "Return the Report Three Information",
+            tags = "REPORT",
+            parameters = {
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "institution",
+                            required = true,
+                            description = "Filter Institution",
+                            example = "00000007"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "serviceContract",
+                            required = true,
+                            description = "Filter Service Contract",
+                            example = "149"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "initialDate",
+                            required = true,
+                            description = "Filter Initial Date",
+                            example = "2020-10-10"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "finalDate",
+                            required = true,
+                            description = "Filter Final Date",
+                            allowEmptyValue = true,
+                            example = "2021-09-11"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "status",
+                            required = true,
+                            description = "Filter Status List",
+                            allowEmptyValue = true,
+                            example = "[PRE1,PRE2]"
+                    )
+            }
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Sucessful Operation",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = DReport3.class)))
+            )
+    })
     @GetMapping(DReport3.NAME)
     public List<DReport3> getReport3(@RequestParam final String institution,
                                      @RequestParam final String serviceContract,
@@ -81,6 +224,57 @@ public class ReportResource {
         return reportService.getReport3(institution, serviceContract, initialDate, finalDate, (Objects.isNull(status) || status.isEmpty()) ? null : status);
     }
 
+    @Operation(
+            summary = "Return the Report Four Information",
+            description = "Return the Report Four Information",
+            tags = "REPORT",
+            parameters = {
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "institution",
+                            required = true,
+                            description = "Filter Institution",
+                            example = "00000007"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "serviceContract",
+                            required = true,
+                            description = "Filter Service Contract",
+                            example = "149"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "initialDate",
+                            required = true,
+                            description = "Filter Initial Date",
+                            example = "2020-10-10"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "finalDate",
+                            required = true,
+                            description = "Filter Final Date",
+                            allowEmptyValue = true,
+                            example = "2021-09-11"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "status",
+                            required = true,
+                            description = "Filter Status List",
+                            allowEmptyValue = true,
+                            example = "[PRE1,PRE2]"
+                    )
+            }
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Sucessful Operation",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = DReport4.class)))
+            )
+    })
     @GetMapping(DReport4.NAME)
     public List<DReport4> getReport4(@RequestParam final String institution,
                                      @RequestParam final String serviceContract,
@@ -91,6 +285,57 @@ public class ReportResource {
         return reportService.getReport4(institution, serviceContract, initialDate, finalDate, (Objects.isNull(status) || status.isEmpty()) ? null : status);
     }
 
+    @Operation(
+            summary = "Return the Report Five Information",
+            description = "Return the Report Five Information",
+            tags = "REPORT",
+            parameters = {
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "institution",
+                            required = true,
+                            description = "Filter Institution",
+                            example = "00000007"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "serviceContract",
+                            required = true,
+                            description = "Filter Service Contract",
+                            example = "149"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "initialDate",
+                            required = true,
+                            description = "Filter Initial Date",
+                            example = "2020-10-10"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "finalDate",
+                            required = true,
+                            description = "Filter Final Date",
+                            allowEmptyValue = true,
+                            example = "2021-09-11"
+                    ),
+                    @Parameter(
+                            in = ParameterIn.QUERY,
+                            name = "status",
+                            required = true,
+                            description = "Filter Status List",
+                            allowEmptyValue = true,
+                            example = "[PRE1,PRE2]"
+                    )
+            }
+    )
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Sucessful Operation",
+                    content = @Content(array = @ArraySchema(schema = @Schema(implementation = DReport5.class)))
+            )
+    })
     @GetMapping(DReport5.NAME)
     public List<DReport5> getReport5(@RequestParam final String institution,
                                      @RequestParam final String serviceContract,
--- src/main/java/com/fiserv/preproposal/api/domain/dtos/DReport2.java
+++ src/main/java/com/fiserv/preproposal/api/domain/dtos/DReport2.java
@@ -1,5 +1,6 @@
 package com.fiserv.preproposal.api.domain.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,17 +14,88 @@ public class DReport2 {
 
     public static final String NAME = "2";
 
+    @Schema(
+            description = "Nome do arquivo",
+            example = "CNT.CRD.MZ.BCX0.MREMPCRE.D20210625.H191504",
+            type = "String"
+    )
     private String filename;
+
+    @Schema(
+            description = "Instituicao",
+            example = "00000007",
+            type = "String"
+    )
     private String institution;
+
+    @Schema(
+            description = "Data de Processamento",
+            example = "30/06/2021 08:06",
+            type = "String"
+    )
     private String processingDate;
+
+    @Schema(
+            description = "Status do arquivo",
+            example = "Lido com sucesso",
+            type = "String"
+    )
     private String fileStatus;
+
+    @Schema(
+            description = "Numero de Propostas",
+            example = "12",
+            type = "number"
+    )
     private Long proposals;
+
+    @Schema(
+            description = "Numero de Propostas Rejeitadas pelo Fiserv Online",
+            example = "1",
+            type = "number"
+    )
     private Long onlineRejected;
+
+    @Schema(
+            description = "Numero de Propostas Pendentes pelo Fiserv Online",
+            example = "4",
+            type = "number"
+    )
     private Long pendingInFiservOnline;
+
+    @Schema(
+            description = "Numero de Propostas Pendentes Complemento",
+            example = "3",
+            type = "number"
+    )
     private Long pendingComplement;
+
+    @Schema(
+            description = "Numero de Propostas Com Erro",
+            example = "1",
+            type = "number"
+    )
     private Long preproposalError;
+
+    @Schema(
+            description = "Numero de Propostas Finalizadas",
+            example = "1",
+            type = "number"
+    )
     private Long finished;
+
+    @Schema(
+            description = "Numero de Propostas Canceladas no TMP",
+            example = "1",
+            type = "number"
+    )
     private Long tmpCanceled;
+
+    @Schema(
+            description = "Numero de Propostas no Cred Online",
+            example = "1",
+            type = "number"
+    )
     private Long credOnline;
 
 }
